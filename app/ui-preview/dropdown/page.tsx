0465773ce24cec84572283e7b23c837612fd1d0a
Muhammad Atpur Rafif <muhammadatpurrafif@gmail.com>
2023-10-25 23:21:11 +0700

    feat(dropdown): Added preview page for component

---
+++ app/ui-preview/dropdown/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import {
+  Select,
+  SelectTrigger,
+  SelectValue,
+  SelectContent,
+  SelectItem,
+} from "@/components/ui/select";
+
+export default function () {
+  return (
+    <div className="flex flex-col justify-center items-center w-screen h-screen">
+      <Select>
+        <SelectTrigger className="w-[300px]">
+          <SelectValue placeholder="Theme" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="light">Light</SelectItem>
+          <SelectItem value="dark">Dark</SelectItem>
+          <SelectItem value="system">System</SelectItem>
+        </SelectContent>
+      </Select>
+    </div>
+  );
+}
