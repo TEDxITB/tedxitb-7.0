45f6b5fa4ba3e50d8ca4311fd966a8ce4a191407
Nyoman-Gana <nyomanganadipa@gmail.com>
2023-10-27 01:00:51 +0700

    Add: input

---
+++ app/(gana)/Temporary/page.tsx
@@ -0,0 +1,14 @@
+import { Input } from "@/components/ui/input";
+
+import React from "react";
+
+const Temp = () => {
+  return (
+    <div>
+      Temp
+      <Input />
+    </div>
+  );
+};
+
+export default Temp;
