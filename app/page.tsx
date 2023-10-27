c08503f1589f8aece579f28c28ef577084fd43b9
Otzzu <mesachharmasendro@gmail.com>
2023-10-27 22:00:16 +0700

    feat: Add file upload component

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import WordCloud from "./coming-soon-client";
 import { Metadata } from "next";
+import FileUpload from "@/components/file-upload";
 
 export const metadata: Metadata = {
   title: "TEDxITB 7.0",
