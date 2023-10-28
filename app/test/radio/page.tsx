26b4ef738b8e9f2ebf83f13d56af0c5176ec3baf
Lint Action <lint-action@samuelmeuli.com>
2023-10-28 12:34:30 +0000

    Fix code style issues with Prettier

--- app/test/radio/page.tsx
+++ app/test/radio/page.tsx
@@ -1,14 +1,14 @@
 import { RadioGroupDemo } from "@/components/ui/radio";
 
 export default function test() {
-    return (
-      <div className="bg-white w-full min-h-screen flex justify-center items-center flex-col gap-y-4">
-        <RadioGroupDemo color="ted-black" label="table"></RadioGroupDemo>
-        <RadioGroupDemo color="ted-black" label="table"></RadioGroupDemo>
-        <RadioGroupDemo color="ted-red" label="table"></RadioGroupDemo>
-        <RadioGroupDemo color="ted-red" label="table"></RadioGroupDemo>
-        <RadioGroupDemo color="ted-white" label="table"></RadioGroupDemo>
-        <RadioGroupDemo color="ted-white" label="table"></RadioGroupDemo>
-      </div>
-    );
+  return (
+    <div className="flex min-h-screen w-full flex-col items-center justify-center gap-y-4 bg-white">
+      <RadioGroupDemo color="ted-black" label="table"></RadioGroupDemo>
+      <RadioGroupDemo color="ted-black" label="table"></RadioGroupDemo>
+      <RadioGroupDemo color="ted-red" label="table"></RadioGroupDemo>
+      <RadioGroupDemo color="ted-red" label="table"></RadioGroupDemo>
+      <RadioGroupDemo color="ted-white" label="table"></RadioGroupDemo>
+      <RadioGroupDemo color="ted-white" label="table"></RadioGroupDemo>
+    </div>
+  );
 }
