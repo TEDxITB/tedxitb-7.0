6bcd8bb744ee9a3c85a3213b408ad35d27489010
Muhammad Atpur Rafif <muhammadatpurrafif@gmail.com>
2023-10-28 18:04:56 +0700

    feat(dropdown):ui preview

---
+++ app/test/dropdown/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import {
+  Select,
+  SelectTrigger,
+  SelectValue,
+  SelectContent,
+  SelectItem,
+} from "@/components/ui/select";
+
+export default function Page() {
+  return (
+    <div className="flex h-screen w-screen flex-col items-center justify-center bg-white">
+      <Select>
+        <SelectTrigger className="w-[300px]" label="Label">
+          <SelectValue placeholder="Select Option" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="light">Option</SelectItem>
+          <SelectItem value="dark">Option</SelectItem>
+          <SelectItem value="system">Option</SelectItem>
+        </SelectContent>
+      </Select>
+    </div>
+  );
+}
