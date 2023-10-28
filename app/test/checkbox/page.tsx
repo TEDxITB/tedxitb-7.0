d0bd2d1994240fabfccaa9248052b9ed743a37c2
Lint Action <lint-action@samuelmeuli.com>
2023-10-28 12:47:18 +0000

    Fix code style issues with Prettier

--- app/test/checkbox/page.tsx
+++ app/test/checkbox/page.tsx
@@ -1,11 +1,11 @@
 import { CheckboxWithText } from "@/components/ui/checkboxcustom";
 
-export default function test  () {
-    return (
-      <main className="flex min-h-screen flex-col items-center justify-center gap-y-8 overflow-hidden bg-white p-2">
-        <CheckboxWithText label="lable" isDisabled={true}></CheckboxWithText>
-        <CheckboxWithText label="lable" isDisabled={false}></CheckboxWithText>
-        <CheckboxWithText label="lable" isDisabled={false}></CheckboxWithText>
-      </main>
-    );
+export default function test() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center gap-y-8 overflow-hidden bg-white p-2">
+      <CheckboxWithText label="lable" isDisabled={true}></CheckboxWithText>
+      <CheckboxWithText label="lable" isDisabled={false}></CheckboxWithText>
+      <CheckboxWithText label="lable" isDisabled={false}></CheckboxWithText>
+    </main>
+  );
 }
