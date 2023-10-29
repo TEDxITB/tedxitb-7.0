9f28b20af048673150fcab75b523928d52fc60ee
Lint Action <lint-action@samuelmeuli.com>
2023-10-29 13:06:54 +0000

    Fix code style issues with Prettier

--- components/ui/cards.tsx
+++ components/ui/cards.tsx
@@ -1,6 +1,6 @@
-import * as React from "react"
+import * as React from "react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 const Card = React.forwardRef<
   HTMLDivElement,
@@ -14,8 +14,8 @@ const Card = React.forwardRef<
     )}
     {...props}
   />
-))
-Card.displayName = "Card"
+));
+Card.displayName = "Card";
 
 const CardImage = React.forwardRef<
   HTMLImageElement,
@@ -23,7 +23,7 @@ const CardImage = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <img
     ref={ref}
-    className={cn("w-full h-auto rounded-t-lg", className)}
+    className={cn("h-auto w-full rounded-t-lg", className)}
     {...props}
   />
 ));
@@ -35,14 +35,11 @@ const CardTitle = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <h3
     ref={ref}
-    className={cn(
-      "text-[30px] font-normal",
-      className
-    )}
+    className={cn("text-[30px] font-normal", className)}
     {...props}
   />
-))
-CardTitle.displayName = "CardTitle"
+));
+CardTitle.displayName = "CardTitle";
 
 const CardDescription = React.forwardRef<
   HTMLParagraphElement,
@@ -50,19 +47,22 @@ const CardDescription = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <p
     ref={ref}
-    className={cn("text-[18px] text-justify mt-[14px]", className)}
+    className={cn("mt-[14px] text-justify text-[18px]", className)}
     {...props}
   />
-))
-CardDescription.displayName = "CardDescription"
+));
+CardDescription.displayName = "CardDescription";
 
 const CardContent = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn("mt-[19px] mb-[47px] mx-[20px]", className)} {...props} />
-))
-CardContent.displayName = "CardContent"
+  <div
+    ref={ref}
+    className={cn("mx-[20px] mb-[47px] mt-[19px]", className)}
+    {...props}
+  />
+));
+CardContent.displayName = "CardContent";
 
-
-export { Card, CardTitle, CardDescription, CardContent, CardImage }
+export { Card, CardTitle, CardDescription, CardContent, CardImage };
