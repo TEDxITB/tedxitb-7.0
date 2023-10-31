8d8adf4a8be319c668c274a315e6bb76315bd985
randyver <randyverdian99@gmail.com>
2023-10-31 08:29:37 +0700

    fix(button): Change hover

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -11,9 +11,9 @@ const buttonVariants = cva(
       variant: {
         primary: "bg-ted-red text-primary-foreground hover:bg-ted-red/90",
         secondary: "bg-ted-opaque-red text-ted-red hover:bg-ted-opaque-red/90",
-        outline: "bg-white border border-solid border-ted-red",
+        outline: "bg-white border border-solid border-ted-red hover:bg-white/90",
         ghost: "bg-white hover:bg-white/90",
-        link: "text-ted-red hover:underline underline-ffset-4",
+        link: "text-ted-red hover:underline underline-offset-4",
       },
       size: {
         "default-rounded": "rounded-full h-10 px-4 py-2",
