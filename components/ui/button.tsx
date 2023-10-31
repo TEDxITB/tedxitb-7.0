38f1c4920fb518c266c396c9e8270ee470eeb363
Randy Verdian <116436617+randyver@users.noreply.github.com>
2023-10-31 09:33:35 +0700

    Button (#8)
    * feat(button): Add button
    
    * Fix code style issues with Prettier
    
    * fix(button): Change variant and size
    
    * Fix code style issues with Prettier
    
    * feat(button): Add test disabled button
    
    * fix(button): Add hover
    
    * fix(button): Change hover
    
    * Fix code style issues with Prettier
    
    ---------
    
    Co-authored-by: Lint Action <lint-action@samuelmeuli.com>

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -9,25 +9,26 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
-        destructive:
-          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
+        primary: "bg-ted-red text-primary-foreground hover:bg-ted-red/90",
+        secondary: "bg-ted-opaque-red text-ted-red hover:bg-ted-opaque-red/90",
         outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
-        secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
+          "bg-white border border-solid border-ted-red hover:bg-white/90",
+        ghost: "bg-white hover:bg-white/90",
+        link: "text-ted-red hover:underline underline-offset-4",
       },
       size: {
-        default: "h-10 px-4 py-2",
+        "default-rounded": "rounded-full h-10 px-4 py-2",
+        "sm-rounded": "rounded-full h-9 px-3",
+        "lg-rounded": "rounded-full h-11 px-6",
+        "icon-rounded": "rounded-full h-10 w-10",
+        default: "h-10 rounded-md px-4 py-2",
         sm: "h-9 rounded-md px-3",
-        lg: "h-11 rounded-md px-8",
-        icon: "h-10 w-10",
+        lg: "h-11 px-6",
+        icon: "aspect-square h-10 w-10 rounded-md",
       },
     },
     defaultVariants: {
-      variant: "default",
+      variant: "primary",
       size: "default",
     },
   }
