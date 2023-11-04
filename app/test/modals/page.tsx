7322a09bba8566b0182c48c71b38c05aa8d780a6
Lint Action <lint-action@samuelmeuli.com>
2023-11-04 17:16:11 +0000

    Fix code style issues with Prettier

--- app/test/modals/page.tsx
+++ app/test/modals/page.tsx
@@ -3,11 +3,16 @@ import React from "react";
 
 const page = () => {
   return (
-    <div className="flex justify-center items-center bg-ted-white min-h-screen w-full">
-        {/* <Modals variant="type-1" modalTitle="Modal Title" modalDescription="Lorem ipsum dolot sit amet" modalSubDescription="Dummy sub description"></Modals> */}
-        <Modals variant="type-2" modalTitle="Modal Title" modalDescription="Lorem ipsum dolot sit amet" modalSubDescription="Dummy sub description"></Modals>
-        {/* <Modals variant="type-3" modalTitle="Modal Title" modalDescription="Lorem ipsum dolot sit amet" modalSubDescription="Dummy sub description"></Modals> */}
-        {/* <Modals variant="type-4" modalTitle="Modal Title" modalDescription="Lorem ipsum dolot sit amet" modalSubDescription="Dummy sub description"></Modals> */}
+    <div className="flex min-h-screen w-full items-center justify-center bg-ted-white">
+      {/* <Modals variant="type-1" modalTitle="Modal Title" modalDescription="Lorem ipsum dolot sit amet" modalSubDescription="Dummy sub description"></Modals> */}
+      <Modals
+        variant="type-2"
+        modalTitle="Modal Title"
+        modalDescription="Lorem ipsum dolot sit amet"
+        modalSubDescription="Dummy sub description"
+      ></Modals>
+      {/* <Modals variant="type-3" modalTitle="Modal Title" modalDescription="Lorem ipsum dolot sit amet" modalSubDescription="Dummy sub description"></Modals> */}
+      {/* <Modals variant="type-4" modalTitle="Modal Title" modalDescription="Lorem ipsum dolot sit amet" modalSubDescription="Dummy sub description"></Modals> */}
     </div>
   );
 };
--- components/ui/modals.tsx
+++ components/ui/modals.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import Image from "next/image";
 
 interface ModalsProps {
-    variant: 'type-1' | 'type-2' | 'type-3' | 'type-4';
-    modalTitle : string;
-    modalDescription : string;
-    modalSubDescription : string;
+  variant: "type-1" | "type-2" | "type-3" | "type-4";
+  modalTitle: string;
+  modalDescription: string;
+  modalSubDescription: string;
 }
 
 const backgroundImages = {
@@ -16,56 +16,60 @@ const backgroundImages = {
 };
 
 const titleColorType = {
-    'type-1': 'ted-red',
-    'type-2': 'ted-white',
-    'type-3': 'ted-white',
-    'type-4': 'ted-white',
+  "type-1": "ted-red",
+  "type-2": "ted-white",
+  "type-3": "ted-white",
+  "type-4": "ted-white",
 };
 
 const descriptionColorType = {
-    'type-1': 'ted-black',
-    'type-2': 'ted-white',
-    'type-3': 'ted-white',
-    'type-4': 'ted-white',
+  "type-1": "ted-black",
+  "type-2": "ted-white",
+  "type-3": "ted-white",
+  "type-4": "ted-white",
 };
 
-const Modals: React.FC<ModalsProps> = ({ variant, modalTitle, modalDescription, modalSubDescription }) => {
-    const titleColor = titleColorType[variant];
-    const descriptionColor = descriptionColorType[variant];
+const Modals: React.FC<ModalsProps> = ({
+  variant,
+  modalTitle,
+  modalDescription,
+  modalSubDescription,
+}) => {
+  const titleColor = titleColorType[variant];
+  const descriptionColor = descriptionColorType[variant];
 
-    return (
-      <div
-        className={`h-[375px] w-[600px] border border-ted-black border-opacity-25 rounded-[15px]`}
-      >
-        <Image
-          src={`${backgroundImages[variant]}`}
-          alt={"backgroundImage"}
-          className={`min-w-full ${
-            variant == "type-1" ? "rounded-t-[15px]" : "rounded-[15px]"
-          }`}
-          width={400}
-          height={400}
-        ></Image>
-        <div className="absolute inset-0 flex flex-col items-center justify-center gap-y-4">
-          <h2
-            className={`text-${titleColor} font-anderson text-[48px] font-[700] leading-[59px]`}
-          >
-            {modalTitle}
-          </h2>
-          <p
-            className={`text-${descriptionColor} font-anderson text-[24px] font-[400] leading-[30px]`}
-          >
-            {modalDescription}
-          </p>
-          <p
-            className={`font-anderson text-[16px] font-[400] leading-[20px] text-[#CACACA]`}
-          >
-            {modalSubDescription}
-          </p>
-        </div>
+  return (
+    <div
+      className={`h-[375px] w-[600px] rounded-[15px] border border-ted-black border-opacity-25`}
+    >
+      <Image
+        src={`${backgroundImages[variant]}`}
+        alt={"backgroundImage"}
+        className={`min-w-full ${
+          variant == "type-1" ? "rounded-t-[15px]" : "rounded-[15px]"
+        }`}
+        width={400}
+        height={400}
+      ></Image>
+      <div className="absolute inset-0 flex flex-col items-center justify-center gap-y-4">
+        <h2
+          className={`text-${titleColor} font-anderson text-[48px] font-[700] leading-[59px]`}
+        >
+          {modalTitle}
+        </h2>
+        <p
+          className={`text-${descriptionColor} font-anderson text-[24px] font-[400] leading-[30px]`}
+        >
+          {modalDescription}
+        </p>
+        <p
+          className={`font-anderson text-[16px] font-[400] leading-[20px] text-[#CACACA]`}
+        >
+          {modalSubDescription}
+        </p>
       </div>
-    );
+    </div>
+  );
 };
-
 
 export default Modals;
