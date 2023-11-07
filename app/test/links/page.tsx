46d2d10341ec406ef3a9d3f8bb04f9a7ad56ebc4
Lint Action <lint-action@samuelmeuli.com>
2023-11-07 00:09:45 +0000

    Fix code style issues with Prettier

--- app/test/links/page.tsx
+++ app/test/links/page.tsx
@@ -1,11 +1,11 @@
 import Links from "@/components/ui/links";
 
 const Test = () => {
-    return (
-        <div className="flex justify-center items-center bg-white min-h-screen w-full flex-col  ">
-            <Links label="LinkLinkLinkLink" isError={false}></Links>
-        </div>
-    )
-}
+  return (
+    <div className="flex min-h-screen w-full flex-col items-center justify-center bg-white  ">
+      <Links label="LinkLinkLinkLink" isError={false}></Links>
+    </div>
+  );
+};
 
 export default Test;
--- components/ui/links.tsx
+++ components/ui/links.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import { useState } from "react";
 
 interface linkProps {
@@ -6,61 +6,60 @@ interface linkProps {
   isError?: boolean;
 }
 
-
 const Links: React.FC<linkProps> = ({ label, isError }) => {
-    const [isVisited, setIsVisited] = useState(false);
-    const handleClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
-        if (isError) {
-        event.preventDefault();
-        } else {
-        setIsVisited(true);
-        }
-    };
+  const [isVisited, setIsVisited] = useState(false);
+  const handleClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    if (isError) {
+      event.preventDefault();
+    } else {
+      setIsVisited(true);
+    }
+  };
 
-    return (
-        <a
-        href="https://www.github.com"
-        onClick={handleClick}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="group"
+  return (
+    <a
+      href="https://www.github.com"
+      onClick={handleClick}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="group"
+    >
+      <div
+        className={`flex flex-row items-center gap-x-4 ${
+          isError ? "cursor-not-allowed" : "cursor-pointer"
+        }`}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          className={`  ${isVisited ? "text-[#3F1871]" : "text-[#0079FF]"} ${
+            isError ? "text-[#FF0000]" : "group-hover:text-[#004797]"
+          }`}
         >
-        <div
-            className={`flex flex-row items-center gap-x-4 ${
-            isError ? "cursor-not-allowed" : "cursor-pointer"
-            }`}
+          <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71" />
+          <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71" />
+        </svg>
+        <label
+          className={`font-anderson text-[24px] ${
+            isVisited ? "text-[#3F1871]" : "text-[#0079FF]"
+          }  ${
+            isError
+              ? "text-[#FF0000] hover:cursor-not-allowed"
+              : "hover:cursor-pointer group-hover:text-[#004797] group-hover:underline group-hover:underline-offset-1"
+          } `}
         >
-            <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            className={`  ${isVisited ? "text-[#3F1871]" : "text-[#0079FF]"} ${
-                isError ? "text-[#FF0000]" : "group-hover:text-[#004797]"
-            }`}
-            >
-            <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71" />
-            <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71" />
-            </svg>
-            <label
-            className={`font-anderson text-[24px] ${
-                isVisited ? "text-[#3F1871]" : "text-[#0079FF]"
-            }  ${
-                isError
-                ? "text-[#FF0000] hover:cursor-not-allowed"
-                : "hover:cursor-pointer group-hover:text-[#004797] group-hover:underline group-hover:underline-offset-1"
-            } `}
-            >
-            {label}
-            </label>
-        </div>
-        </a>
-    );
+          {label}
+        </label>
+      </div>
+    </a>
+  );
 };
 
 export default Links;
