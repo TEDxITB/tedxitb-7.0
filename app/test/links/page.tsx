72c3be09388e52e96bbbf8b38de0cd5615cc290f
ChaiGans <elbertchailes888@gmail.com>
2023-11-07 07:08:32 +0700

    add: feature links

---
+++ app/test/links/page.tsx
@@ -0,0 +1,11 @@
+import Links from "@/components/ui/links";
+
+const Test = () => {
+    return (
+        <div className="flex justify-center items-center bg-white min-h-screen w-full flex-col  ">
+            <Links label="LinkLinkLinkLink" isError={false}></Links>
+        </div>
+    )
+}
+
+export default Test;
