c4165fc9d42c464f627abdf5e6cb38388ab5ccda
ChaiGans <elbertchailes888@gmail.com>
2023-11-07 23:19:37 +0700

    add: label as children, ..props, a to Link

--- app/test/links/page.tsx
+++ app/test/links/page.tsx
@@ -2,10 +2,12 @@ import Links from "@/components/ui/links";
 
 const Test = () => {
     return (
-        <div className="flex justify-center items-center bg-white min-h-screen w-full flex-col  ">
-            <Links label="LinkLinkLinkLink" isError={false}></Links>
-        </div>
-    )
+      <div className="flex min-h-screen w-full flex-col items-center justify-center bg-white  ">
+        <Links href="https://www.github.com/ChaiGans" target="_blank">
+          LinkLinkLinkLink
+        </Links>
+      </div>
+    );
 }
 
 export default Test;
--- components/ui/links.tsx
+++ components/ui/links.tsx
@@ -1,66 +1,28 @@
-'use client'
-import { useState } from "react";
+import Link from "next/link";
+import { Link as IconLink } from "lucide-react";
 
 interface linkProps {
-  label: string;
-  isError?: boolean;
+  target: string;
+  href: string;
+  children?: React.ReactNode;
 }
 
-
-const Links: React.FC<linkProps> = ({ label, isError }) => {
-    const [isVisited, setIsVisited] = useState(false);
-    const handleClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
-        if (isError) {
-        event.preventDefault();
-        } else {
-        setIsVisited(true);
-        }
-    };
-
-    return (
-        <a
-        href="https://www.github.com"
-        onClick={handleClick}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="group"
-        >
-        <div
-            className={`flex flex-row items-center gap-x-4 ${
-            isError ? "cursor-not-allowed" : "cursor-pointer"
-            }`}
-        >
-            <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            className={`  ${isVisited ? "text-[#3F1871]" : "text-[#0079FF]"} ${
-                isError ? "text-[#FF0000]" : "group-hover:text-[#004797]"
-            }`}
-            >
-            <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71" />
-            <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71" />
-            </svg>
-            <label
-            className={`font-anderson text-[24px] ${
-                isVisited ? "text-[#3F1871]" : "text-[#0079FF]"
-            }  ${
-                isError
-                ? "text-[#FF0000] hover:cursor-not-allowed"
-                : "hover:cursor-pointer group-hover:text-[#004797] group-hover:underline group-hover:underline-offset-1"
-            } `}
-            >
-            {label}
-            </label>
-        </div>
-        </a>
-    );
+const Links: React.FC<linkProps> = ({ href, children, target, ...props }) => {
+  return (
+    <Link
+      href={href}
+      target={target}
+      {...props}
+      className="visited:text-[#3F1871] group"
+    >
+      <div className="flex cursor-pointer flex-row items-center gap-x-4">
+        <IconLink className="text-[#0079FF] group-hover:text-[#004797]" />
+        <label className="font-anderson text-[24px] text-[#0079FF] hover:cursor-pointer group-hover:text-[#004797] group-hover:underline group-hover:underline-offset-1">
+          {children}
+        </label>
+      </div>
+    </Link>
+  );
 };
 
 export default Links;
