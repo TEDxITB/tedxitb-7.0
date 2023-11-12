a0e6680421cd13576597cd6116e647e5d90a6144
Muhammad Atpur Rafif <66266045+atpur-rafif@users.noreply.github.com>
2023-11-12 21:19:27 +0700

    feat/navbar (#15)
    * feat(header): Desktop version
    
    * fix(prettier): Format document
    
    * Fix code style issues with Prettier
    
    * feat(navbar): Responsive between desktop and mobile
    
    * Fix code style issues with Prettier
    
    * feat(navbar): Recommit losed changed after fixed conflict
    
    * Fix code style issues with Prettier
    
    * feat(navbar): Update based on comment
    
    * Fix code style issues with Prettier
    
    * feat(navbar): Update test name from header to navbar
    
    * feat(navbar): Comment update (2)
    
    * Fix code style issues with Prettier
    
    * feat(navbar): Add gap and change padding to x axis only
    
    * Fix code style issues with Prettier
    
    * feat(navbar): Sticky styling
    
    * Fix code style issues with Prettier
    
    * feat: Added small changes for finishing.
    
    ---------
    
    Co-authored-by: Lint Action <lint-action@samuelmeuli.com>
    Co-authored-by: Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>

---
+++ app/test/navbar/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import NavBar from "@/components/NavBar";
+
+export default function Page() {
+  return (
+    <div className="h-[2000px] w-screen">
+      <NavBar />
+    </div>
+  );
+}
