c67a2e5a35679b5c177f26b09fd112bfc5755c5d
Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>
2023-11-30 12:42:46 +0700

    fix(links): custom link interface.

--- app/test/links/page.tsx
+++ app/test/links/page.tsx
@@ -3,6 +3,9 @@ import CustomLink from "@/components/ui/links";
 const Test = () => {
   return (
     <div className="flex min-h-screen w-full flex-col items-center justify-center bg-white  ">
+      <CustomLink href="https://www.tokopedia.com" target="_blank">
+        LinkLinkLinkLink
+      </CustomLink>
       <CustomLink href="https://www.tokopeda.com" target="_blank">
         LinkLinkLinkLink
       </CustomLink>
--- components/ui/links.tsx
+++ components/ui/links.tsx
@@ -1,29 +1,27 @@
+import * as React from "react";
 import Link from "next/link";
 import { Link as IconLink } from "lucide-react";
+import { LinkProps } from "next/link";
 
-interface linkProps {
-  target: string;
-  href: string;
-  children?: React.ReactNode;
-}
+type CustomLink = LinkProps & {
+  children: string;
+  target: "_blank" | "_self" | "_parent" | "_top";
+};
 
-const CustomLink: React.FC<linkProps> = ({
-  href,
-  children,
-  target,
-  ...props
-}) => {
-  return (
+const CustomLink = React.forwardRef<HTMLAnchorElement, CustomLink>(
+  ({ href, children, ...props }, ref) => (
     <Link
       href={href}
-      target={target}
+      ref={ref}
+      className="group flex flex-row items-center gap-x-2 text-[#0079FF] visited:text-[#551A8B] hover:text-[#0056B2] hover:underline hover:underline-offset-2 visited:hover:text-[#361059]"
       {...props}
-      className="group flex flex-row items-center gap-x-2 text-[#0079FF] visited:text-[#3F1871] hover:text-[#004797] hover:underline hover:underline-offset-2"
     >
       <IconLink className="w-4" />
-      <label className="cursor-pointer font-anderson text-lg">{children}</label>
+      <span className="cursor-pointer font-anderson text-lg">{children}</span>
     </Link>
-  );
-};
+  )
+);
+
+CustomLink.displayName = "CustomLink";
 
 export default CustomLink;
