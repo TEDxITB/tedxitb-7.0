24c43fc8d4ff138b17990ac867af445ab73e1293
Randy Verdian <116436617+randyver@users.noreply.github.com>
2023-11-30 16:57:36 +0700

    Toasts (#17)
    * feat(button): Add button
    
    * Fix code style issues with Prettier
    
    * fix(button): Change variant and size
    
    * Fix code style issues with Prettier
    
    * feat(button): Add test disabled button
    
    * fix(button): Add hover
    
    * fix(button): Change hover
    
    * Fix code style issues with Prettier
    
    * fix(toast): package toast
    
    * feat(breadcrumbs): Add breadcrumbs
    
    * feat(toasts): change style and add icon props
    
    * Fix code style issues with Prettier
    
    * fix: Delete conflict file
    
    * fix: change size icon and strip
    
    * fix: change margin
    
    * fix: change height and delete border
    
    * fix(toasts): change rounded
    
    * fix: Toasts svg and colors.
    
    * Fix code style issues with Prettier
    
    ---------
    
    Co-authored-by: Lint Action <lint-action@samuelmeuli.com>
    Co-authored-by: Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>
    Co-authored-by: Dewantoro Triatmojo <99950492+dewodt@users.noreply.github.com>

---
+++ components/ui/toaster.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import {
+  Toast,
+  ToastClose,
+  ToastDescription,
+  ToastProvider,
+  ToastTitle,
+  ToastViewport,
+} from "@/components/ui/toast";
+import { useToast } from "@/components/ui/use-toast";
+import Image from "next/image";
+
+export function Toaster() {
+  const { toasts } = useToast();
+
+  const variantIcons: Record<string, string> = {
+    error: "/ErrorIcon.svg",
+    success: "/SuccessIcon.svg",
+    profile: "/ProfileIcon.svg",
+    warning: "/WarningIcon.svg",
+    loading: "/LoadingIcon.svg",
+  };
+
+  return (
+    <ToastProvider>
+      {toasts.map(function ({
+        id,
+        title,
+        description,
+        action,
+        variant,
+        icon,
+        ...props
+      }) {
+        const variantStyles: Record<string, string> = {
+          error: "bg-[#E63E32] w-3 text-foreground",
+          success: "bg-[#2BB673] w-3 text-foreground",
+          profile: "bg-[#FCBF0E] w-3 text-foreground",
+          warning: "bg-[#2E86E8] w-3 text-foreground",
+          loading: "bg-[#4C4C4C] w-3 text-foreground",
+        };
+
+        const variantStyle = variant ? variantStyles[variant] || "" : "";
+        const typeVariant = variant as string;
+        const shouldShowIcon = icon == true;
+        const iconClass =
+          shouldShowIcon && variant === "loading" && "animate-spin ";
+
+        return (
+          <Toast key={id} {...props}>
+            <div className={variantStyle} />
+            <div className="mx-5 my-6 flex flex-row items-center gap-4">
+              {shouldShowIcon && (
+                <Image
+                  src={variantIcons[typeVariant] || variantIcons.default}
+                  alt={typeVariant}
+                  className={`${iconClass}`}
+                  width={36}
+                  height={36}
+                />
+              )}
+              <div className="flex flex-col gap-1">
+                {title && <ToastTitle>{title}</ToastTitle>}
+                {description && (
+                  <ToastDescription>{description}</ToastDescription>
+                )}
+              </div>
+            </div>
+            {action}
+            <ToastClose />
+          </Toast>
+        );
+      })}
+      <ToastViewport />
+    </ToastProvider>
+  );
+}
