a045370756a85de70d09c31bab46bdaf432ea90a
Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>
2024-01-14 00:07:23 +0700

    add user id in sheets.

--- app/api/registration/route.ts
+++ app/api/registration/route.ts
@@ -137,11 +137,12 @@ export const POST = async (req: NextRequest) => {
 
   // Update google sheets
   const sheetId = process.env.GOOGLE_SHEET_ID as string;
-  const sheetRange = "registration!A:T";
+  const sheetRange = "registration!A:U";
   const sheetValueInputOption = "RAW";
   const values = [
     [
       registrationResult.id,
+      session.id,
       name,
       session.email,
       phone,
