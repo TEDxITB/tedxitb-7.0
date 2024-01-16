e6f0d5716091c2265ff0bf36a7ba7819f038f5f1
Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>
2024-01-17 01:51:37 +0700

    Added posthog rewrite to prevent adblock.

--- next.config.js
+++ next.config.js
@@ -14,7 +14,22 @@ const nextConfig = {
         port: "",
         pathname: "/**/*",
       },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/**/*",
+      },
     ],
+  },
+  async rewrites() {
+    return [
+      // Rewrite to prevent adblocks blocking PostHog
+      {
+        source: "/ingest/:path*",
+        destination: "https://app.posthog.com/:path*",
+      },
+    ];
   },
 };
 
