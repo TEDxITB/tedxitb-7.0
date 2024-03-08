5c731df60b819c4a6408f0c987258d4f9c4d4ca8
Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>
2024-03-08 21:09:29 +0700

    add authorized verifier.

--- lib/authorized-verify-email.ts
+++ lib/authorized-verify-email.ts
@@ -3,4 +3,8 @@ export const AUTHORIZED_VERIFY_EMAIL = [
   "netaniajoanna@gmail.com",
   "syafannisaanindya@gmail.com",
   "dozhoe@gmail.com",
+  "karensabbath04@gmail.com",
+  "muhammadatpurrafif@gmail.com",
+  "meylananggraenii@gmail.com",
+  "moira.moi117@gmail.com",
 ];
