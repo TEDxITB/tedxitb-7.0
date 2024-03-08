cedd2449d34aac1017d7f3f298a05a7af3ceebd3
Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>
2024-03-08 19:28:20 +0700

    feat: loading page, audio hook.

---
+++ app/admin/verify-ticket/loading.tsx
@@ -0,0 +1,7 @@
+import MainEventLoading from "@/app/main-event/main-event-loading";
+
+const VerifyTicketLoadingPage = () => {
+  return <MainEventLoading />;
+};
+
+export default VerifyTicketLoadingPage;
